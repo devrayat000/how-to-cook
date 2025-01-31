ed2085c59cf2df44b65ff47ad26d82a95c4b8681
Zul Ikram Musaddik Rayat <rayathos04@gmail.com>
2025-02-01 00:07:35 +0600

    dependency version mismatch

--- components/ui/link/index.web.tsx
+++ components/ui/link/index.web.tsx
@@ -1,22 +1,17 @@
 import { View } from "react-native";
-import { Children, cloneElement, Fragment } from "react";
+import { Children, Fragment } from "react";
 import { Link, LinkProps } from "expo-router";
 import { A } from "@expo/html-elements";
-import { useTheme } from "@/hooks/useThemeColor";
 
 export default function ThemedLink({ children, ...props }: LinkProps) {
   const isChildrenArray = Children.count(children) > 1;
-  const Wrapper = isChildrenArray
-    ? cloneElement(View, {
-        className: "w-full web:block",
-      })
-    : Fragment;
-  const theme = useTheme();
+  const Wrapper = isChildrenArray ? View : Fragment;
+  const innerProps = !isChildrenArray ? {} : { className: "w-full" };
 
   return (
     <Link {...props} asChild>
       <A>
-        <Wrapper>{children}</Wrapper>
+        <Wrapper {...innerProps}>{children}</Wrapper>
       </A>
     </Link>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { twMerge } from "tw-merge";
+import { twMerge } from "tailwind-merge";
 import { clsx, ClassValue } from "clsx";
 
 export function cn(...classValues: ClassValue[]) {
